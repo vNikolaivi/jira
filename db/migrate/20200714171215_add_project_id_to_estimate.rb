35e66f930c35683f6c30dd4a8a6c2463201002b1
nikolaivi <vnikolaivi@gmail.com>
2020-07-22 22:34:09 +0200

    First commit

--- db/migrate/20200714171215_add_project_id_to_estimate.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectIdToEstimate < ActiveRecord::Migration[5.2]
-  def change
-	add_column :estimates, :project_id, :integer
-  end
-end
---
+++ db/migrate/20200722202925_drop_table_estimates.rb
@@ -0,0 +1,4 @@
+class DropTableEstimates < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
